ac8f24db796c8600af357bdd542feed792cf34ea
Gregor Božnar <gregor.boznar@gmail.com>
2025-08-03 22:37:50 +0200

    refactor: clean up and organize Vite configuration for improved readability and maintainability

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,33 @@
-import vue from '@vitejs/plugin-vue';
-import laravel from 'laravel-vite-plugin';
-import path from 'path';
-import tailwindcss from "@tailwindcss/vite";
-import { resolve } from 'node:path';
-import { defineConfig } from 'vite';
-import svgLoader from 'vite-svg-loader';
-
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/js/app.ts'],
-            ssr: 'resources/js/ssr.ts',
-            refresh: true,
-        }),
-        tailwindcss(),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-        svgLoader(),
-    ],
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        hmr: {
-            host: 'localhost',
+  base: '/',
+  plugins: [
+    laravel({
+      input: ['resources/js/app.ts'],
+      ssr: 'resources/js/ssr.ts',
+      refresh: true,
+    }),
+    tailwindcss(),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
         },
+      },
+    }),
+    svgLoader(),
+  ],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    hmr: {
+      host: 'portal.glab.si', // change from localhost to your domain for remote HMR if needed
     },
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './resources/js'),
-            'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
-        },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './resources/js'),
+      'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
     },
+  },
 });
